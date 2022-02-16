3e7308edf6cfd5a1d5641828a59b1fc91eb45412
cannelle hennuyer <cannelle.hennuyer@gmail.com>
2022-02-16 15:08:26 +0000

    Rails Yelp MVP Completed

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,34 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:index, :new, :create]
+
+  def index
+    @reviews = Review.where(restaurant_id: @restaurant)
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    @review.save
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+end
